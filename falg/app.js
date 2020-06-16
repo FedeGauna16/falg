91182025fe031e89b7020d8d8d6d3e3729ba4943
Franco Masucco <francomasucco2605@gmail.com>
2020-06-16 19:30:01 -0300

    Update app.js

--- falg/app.js
+++ falg/app.js
@@ -3,11 +3,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var inicioRouter = require('./routes/inicio');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { RSA_X931_PADDING } = require('constants');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,6 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/inicio', inicioRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
