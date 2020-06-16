cd290c8f260560dbf06d500441203d2e4e4c9dc2
Gabriel Carames <49535753+GabrielCarames@users.noreply.github.com>
2020-06-16 19:23:39 -0300

    Update app.js

--- falg/app.js
+++ falg/app.js
@@ -2,12 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
+var inicioRouter = require('./routes/inicio');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { RSA_X931_PADDING } = require('constants');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,6 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/inicio', inicioRouter);
 app.use('/users', usersRouter);
-
+ 
 module.exports = app;
