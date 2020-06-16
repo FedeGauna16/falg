b9a96891c1e8fc9d77e0b0a1f90e5eb7f04b27db
Gabriel Caramés <49535753+GabrielCarames@users.noreply.github.com>
2020-06-16 19:29:11 -0300

    soy yo
    soy yo tambien

--- falg/routes/index.js
+++ falg/routes/index.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var builds =
+[
+  {
+    titulo : "Tachancka Build 2020",
+    descripcion : "Pasate a mirar esta build",
+    usuario: "erickjq10x"
+  },
+]
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  
+   res.send(builds);
 });
 
+router.post('/', function(req, res, next) {
+  console.log(req);
+});
+
+
 module.exports = router;
-
-
--- falg/routes/users.js
+++ falg/routes/users.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var hola = "hlasosputo";
+var builds =
+[
+  {
+    titulo : "Tachancka Build 2020",
+    descripcion : "Pasate a mirar esta build",
+    usuario: "erickjq10x"
+  },
+  {
+    nombre : "lucas",
+    apellido : "traje"
+  }
+]
+var manu = "tachancka"
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(
-    {
-      status: true,
-      response: hola,
-    }
-  );
+   res.send(builds);
 });
+
+router.post('/', function(req, res, next) {
+  console.log(req);
+});
+
 
 module.exports = router;
